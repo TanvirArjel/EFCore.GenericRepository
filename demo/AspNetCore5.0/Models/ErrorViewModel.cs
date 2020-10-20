410995fcad77fd3b9488d67dada33ad67f7d6ef0
Tanvir Ahmad Arjel <tanvirarjel@outlook.com>
2020-10-20 19:22:46 +0600

    Initial commit for version 5.0.0 release.

---
+++ demo/AspNetCore5.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore5._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
