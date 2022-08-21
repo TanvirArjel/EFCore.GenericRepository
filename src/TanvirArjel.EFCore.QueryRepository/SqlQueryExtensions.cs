a473631f544c16bc204cc07d9bf85c71ad91d476
Tanvir Ahmad Arjel <tanvir.arjel@outlook.com>
2022-08-21 19:21:36 +0600

    V5.9.1 has been released.

--- src/TanvirArjel.EFCore.QueryRepository/SqlQueryExtensions.cs
+++ src/TanvirArjel.EFCore.QueryRepository/SqlQueryExtensions.cs
@@ -4,17 +4,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data;
 using System.Data.Common;
 using System.Globalization;
-using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace TanvirArjel.EFCore.GenericRepository
 {
@@ -166,112 +161,6 @@ namespace TanvirArjel.EFCore.GenericRepository
             finally
             {
                 await dbContext.Database.CloseConnectionAsync();
-            }
-        }
-
-        private static async Task<List<T>> GetFromQueryAsync2<T>(
-           this DbContext dbContext,
-           string sql,
-           IEnumerable<object> parameters,
-           CancellationToken cancellationToken = default)
-        {
-            RelationalDataReader relationalDataReader = await dbContext.ExecuteSqlQueryAsync(sql, parameters, cancellationToken);
-            using DbDataReader dr = relationalDataReader.DbDataReader;
-
-            List<T> list = new List<T>();
-            T t = default;
-            while (await dr.ReadAsync(cancellationToken))
-            {
-                if (!(typeof(T).IsPrimitive || typeof(T).Equals(typeof(string))))
-                {
-                    PropertyInfo[] props = typeof(T).GetProperties();
-                    IEnumerable<string> actualNames = dr.GetColumnSchema().Select(o => o.ColumnName);
-                    for (int i = 0; i < props.Length; ++i)
-                    {
-                        PropertyInfo pi = props[i];
-
-                        if (!pi.CanWrite)
-                        {
-                            continue;
-                        }
-
-                        ColumnAttribute ca = pi.GetCustomAttribute(typeof(ColumnAttribute)) as ColumnAttribute;
-                        string name = ca?.Name ?? pi.Name;
-
-                        if (pi == null)
-                        {
-                            continue;
-                        }
-
-                        if (!actualNames.Contains(name))
-                        {
-                            continue;
-                        }
-
-                        object value = dr[name];
-                        Type pt = pi.DeclaringType;
-                        bool nullable = pt.GetTypeInfo().IsGenericType && pt.GetGenericTypeDefinition() == typeof(Nullable<>);
-                        if (value == DBNull.Value)
-                        {
-                            value = null;
-                        }
-
-                        if (value == null && pt.GetTypeInfo().IsValueType && !nullable)
-                        {
-                            value = Activator.CreateInstance(pt);
-                        }
-
-                        pi.SetValue(t, value);
-                    }
-
-                    list.Add(t);
-                }
-                else
-                {
-                    t = (T)Convert.ChangeType(dr[0], typeof(T), CultureInfo.InvariantCulture);
-                    list.Add(t);
-                }
-            }
-
-            return list;
-        }
-
-        private static async Task<RelationalDataReader> ExecuteSqlQueryAsync(
-            this DbContext dbContext,
-            string sql,
-            IEnumerable<object> parameters,
-            CancellationToken cancellationToken = default)
-        {
-            if (dbContext == null)
-            {
-                throw new ArgumentNullException(nameof(dbContext));
-            }
-
-            if (string.IsNullOrWhiteSpace(sql))
-            {
-                throw new ArgumentNullException(nameof(sql));
-            }
-
-            IConcurrencyDetector concurrencyDetector = dbContext.GetService<IConcurrencyDetector>();
-
-            using (concurrencyDetector.EnterCriticalSection())
-            {
-                RawSqlCommand rawSqlCommand = dbContext
-                    .GetService<IRawSqlCommandBuilder>()
-                    .Build(sql, parameters);
-
-                RelationalCommandParameterObject paramObject = new RelationalCommandParameterObject(
-                    dbContext.GetService<IRelationalConnection>(),
-                    rawSqlCommand.ParameterValues,
-                    null,
-                    null,
-                    null);
-
-                RelationalDataReader relationalDataReader = await rawSqlCommand
-                    .RelationalCommand
-                    .ExecuteReaderAsync(paramObject, cancellationToken);
-
-                return relationalDataReader;
             }
         }
     }
